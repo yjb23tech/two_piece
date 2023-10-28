609d1c8e9da70630bf5d1283c2a7f7461a8eadd9
yjb23tech <yjb23tech@gmail.com>
2023-10-28 13:40:40 +0100

    Little by little

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,7 @@
 from Player import Player
+from functions import set_player_name, set_player_age, set_player_home_city
 
-user_player = Player("Yuri Orlov", 29, "Odessa")
+user_player = Player(set_player_name(), set_player_age(), set_player_home_city())
 print(user_player)
+
 

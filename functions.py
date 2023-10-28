9402c2b77937b664c4249825148562c169016f58
yjb23tech <yjb23tech@gmail.com>
2023-10-28 14:15:46 +0100

    Lotsa new ideas

--- functions.py
+++ functions.py
@@ -13,10 +13,10 @@ def set_player_age() -> int:
             chosen_user_age = int(input("\nHow old are you Sailor?\n"))
             return chosen_user_age
         except ValueError:
-            print("You entered the wrong value; try again")
+            print("\nYou entered the wrong value; try again Sailor!")
             continue
     
-    
+#Might be interesting to combine this with some type of geographical API? Work with CRUD or basic API stuff... 
 def set_player_home_city() -> str:
 
     chosen_user_home_city = input("\nAnd where are you from originally Sailor?\n")

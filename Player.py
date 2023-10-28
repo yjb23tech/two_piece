5c7e0b50a06e56f61fc9c744e75fd45b7c92954f
yjb23tech <yjb23tech@gmail.com>
2023-10-28 13:33:33 +0100

    A fair few

---
+++ Player.py
@@ -0,0 +1,14 @@
+class Player:
+
+    def __init__(self, str_name, int_age, str_place_of_birth):
+
+        self.str_name = str_name 
+        self.int_age = int(int_age)
+        self.str_place_of_birth = str_place_of_birth
+
+        self.hp = 100 
+    
+    def __str__(self):
+
+        return (f"\nName: Captain {self.str_name}\nAge: {self.int_age}\nHome City: {self.str_place_of_birth}\n")
+
---
+++ sandbox.py
@@ -0,0 +1,5 @@
+from Player import Player
+
+user_player = Player("Yuri Orlov", 29, "Odessa")
+print(user_player)
+

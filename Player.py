791a92a7c3636724c42b326a1376ea700bf7ddf5
yjb23tech <yjb23tech@gmail.com>
2023-10-29 16:56:23 +0000

    Higher and Higher

--- Player.py
+++ Player.py
@@ -20,8 +20,8 @@ class Player:
     def get_player_current_tile_location(self, arr_world_map_back_end):
         
         player_current_tile_location = arr_world_map_back_end[self.int_loc_x][self.int_loc_y]
-        print(f"You're currently on {player_current_tile_location.str_tile_name} Island in the {player_current_tile_location.str_tile_quadrant} quadrant of the map")
-        print(f"Your current coordinate location is [{self.int_loc_x}, {self.int_loc_y}]")
+        print(f"\nYou're currently on {player_current_tile_location.str_tile_name} Island in the {player_current_tile_location.str_tile_quadrant} quadrant of the map")
+        print(f"Your current coordinate location is [{self.int_loc_x}, {self.int_loc_y}]\n")
         return player_current_tile_location
 
 
